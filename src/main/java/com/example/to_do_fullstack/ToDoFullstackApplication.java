e84617a92614e9d547de66b9b50335f854c87c2f
devlucascalado <devlucascalado@gmail.com>
2025-08-29 21:03:44 -0300

    projeto-configurações

---
+++ src/main/java/com/example/to_do_fullstack/ToDoFullstackApplication.java
@@ -0,0 +1,12 @@
+package com.example.to_do_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoFullstackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/to_do_fullstack/ToDoFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.to_do_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
