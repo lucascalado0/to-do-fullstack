cc0c8fd7a43cde2cd71e889560f1e3e5687bd364
devlucascalado <devlucascalado@gmail.com>
2025-08-30 21:47:55 -0300

    tarefa-repository

--- src/main/java/com/example/to_do_fullstack/repository/TarefaRepository.java
+++ src/main/java/com/example/to_do_fullstack/repository/TarefaRepository.java
@@ -1,4 +1,9 @@
 package com.example.to_do_fullstack.repository;
 
-public interface TarefaRepository {
+import com.example.to_do_fullstack.entity.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 }
