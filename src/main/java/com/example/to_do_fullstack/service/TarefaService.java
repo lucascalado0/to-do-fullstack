fe566ac909b278a586acdba0cfb77bb9694b96e7
devlucascalado <devlucascalado@gmail.com>
2025-08-30 22:13:35 -0300

    metodos-tarefa-service

--- src/main/java/com/example/to_do_fullstack/service/TarefaService.java
+++ src/main/java/com/example/to_do_fullstack/service/TarefaService.java
@@ -1,4 +1,16 @@
 package com.example.to_do_fullstack.service;
 
+import com.example.to_do_fullstack.dto.TarefaDTO;
+
+
+import java.util.List;
+import java.util.Optional;
+
+
 public interface TarefaService {
+    Optional<TarefaDTO> buscarPorId(Long id);
+    TarefaDTO criarTarefa(TarefaDTO tarefaDTO);
+    void deletar(Long id);
+    Optional<TarefaDTO> atualizarTarefa(Long id, TarefaDTO tarefaDTO);
+    List<TarefaDTO> listarTodas();
 }
