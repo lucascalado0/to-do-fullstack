cfd6d46497df29faead3d45b78727f679267dd47
devlucascalado <devlucascalado@gmail.com>
2025-09-04 23:02:23 -0300

    tarefa-mapper

--- src/main/java/com/example/to_do_fullstack/mapper/TarefaMapper.java
+++ src/main/java/com/example/to_do_fullstack/mapper/TarefaMapper.java
@@ -4,13 +4,16 @@ package com.example.to_do_fullstack.mapper;
 import com.example.to_do_fullstack.dto.TarefaDTO;
 import com.example.to_do_fullstack.entity.Tarefa;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
+import java.util.List;
+
+
+@Mapper(componentModel = "spring")
 public interface TarefaMapper {
-    TarefaMapper INSTANCE = Mappers.getMapper(TarefaMapper.class);
 
     TarefaDTO toDTO(Tarefa tarefa);
 
     Tarefa toEntity(TarefaDTO tarefaDTO);
+
+    List<TarefaDTO> toDTOList(List<Tarefa> tarefas);
 }
