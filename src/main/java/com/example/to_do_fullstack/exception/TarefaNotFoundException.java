3290ce4734356b1146dbabdbf3a467e5e5fab082
devlucascalado <devlucascalado@gmail.com>
2025-09-04 23:02:13 -0300

    tarefa-exception

--- src/main/java/com/example/to_do_fullstack/exception/TarefaNotFoundException.java
+++ src/main/java/com/example/to_do_fullstack/exception/TarefaNotFoundException.java
@@ -1,5 +1,9 @@
 package com.example.to_do_fullstack.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class TarefaNotFoundException extends RuntimeException {
     public TarefaNotFoundException(String message) {
         super(message);
